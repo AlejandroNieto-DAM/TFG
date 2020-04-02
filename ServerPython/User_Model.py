057773c459cdd131de2298a7c24d813774cf8730
unknown <alejandrona5b@gmail.com>
2020-04-03 00:31:10 +0200

    server adding classes

---
+++ ServerPython/User_Model.py
@@ -0,0 +1,28 @@
+import pymysql
+
+class User_Model:
+
+    def __init__(self):
+        self.host = "localhost"
+        self.port = "3306"
+        self.user = "root"
+        self.passwd = ""
+        self.db = "tfg"
+
+    def getUserByLoginAndPassword(self, name, password):
+        conn = pymysql.connect('localhost', 'root', '', 'tfg')
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM user WHERE login = '" + name + "' and password = '" + password + "'")
+
+        datos = []
+
+        for row in cur.fetchone():
+            datos.append(row)
+
+        print(datos)
+
+        cur.close()
+        conn.close()
+
+        return datos
+
