6a05909e8f255a94818035072d8ee204fe55409e
Alejandro Nieto Alarcon <alejandrona5b@gmail.com>
2020-05-28 13:42:23 +0200

    reset

--- PruebaClientSwift/Pods/Target Support Files/Pods-prueba5/Pods-prueba5-dummy 2.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_prueba5 : NSObject
-@end
-@implementation PodsDummy_Pods_prueba5
-@end
--- WebTfg/Classes/Device 2.py
+++
@@ -1,7 +0,0 @@
-
-class Device:
-    def __init__(self, id, name, state, maintenante):
-        self.id = id
-        self.name = name
-        self.state = state
-        self.maintenance = maintenante
